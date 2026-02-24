73c3592b7293b5b379d4ba0388bdb17ae98244d7
Marcelo Rodrigo Neves <marcelo.m7.sistemas@gmail.com>
2026-02-24 12:16:08 -0300

    feat: Implementado arquivo readme.mm

---
+++ readme.mm
@@ -0,0 +1,51 @@
+<map version="1.0.10">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1771329307324" ID="ID_745833272" LINK="../../Pessoal/mapasMentais/Conhecimento.mm" MODIFIED="1771945092765" TEXT="XPathWorkbench">
+<icon BUILTIN="licq"/>
+<node CREATED="1771334924247" MODIFIED="1771334931222" POSITION="right" TEXT="Apelido">
+<icon BUILTIN="Descriptor.grouping"/>
+<node CREATED="1771334931915" MODIFIED="1771334942048" TEXT="m7-xpath">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+<node CREATED="1768142021485" MODIFIED="1768162459452" POSITION="right" TEXT="Vers&#xe3;o java utilizada no projeto">
+<icon BUILTIN="Descriptor.grouping"/>
+<node CREATED="1768157244050" MODIFIED="1768157486742" TEXT="sdk use java 21.0.1-tem">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+<node CREATED="1769900121558" MODIFIED="1769900137970" POSITION="right" TEXT="Comandos executado na constru&#xe7;&#xe3;o do projeto">
+<icon BUILTIN="Descriptor.grouping"/>
+<node CREATED="1769900185812" MODIFIED="1771329958831" TEXT="mvn archetype:generate -DgroupId=com.m7sistemas.xpathworkbench -DartifactId=xpath-workbench -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+<node CREATED="1771334521343" MODIFIED="1771334885582" POSITION="right" TEXT="Para n&#xe3;o dar erro no vscode, foi mudado o java padr&#xe3;o do sdkman:">
+<icon BUILTIN="Descriptor.grouping"/>
+<node CREATED="1771334555002" MODIFIED="1771334890588" TEXT="sdk default java 21.0.1-tem">
+<icon BUILTIN="tag_green"/>
+</node>
+<node CREATED="1771334581707" MODIFIED="1771334598050" TEXT="Caso tenha que voltar a vers&#xe3;o, executar o seguinte comando:">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1771334555002" MODIFIED="1771335194509" TEXT="sdk default java 8.0.382-amzn">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+<node CREATED="1771333442723" MODIFIED="1771443922088" POSITION="right" TEXT="Executar para teste">
+<icon BUILTIN="Descriptor.grouping"/>
+<node CREATED="1771333450564" MODIFIED="1771333454471" TEXT="mvn clean javafx:run">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+<node CREATED="1771446381175" MODIFIED="1771446385810" POSITION="right" TEXT="Gerar executavel">
+<icon BUILTIN="Descriptor.grouping"/>
+<node CREATED="1771446386248" MODIFIED="1771683142501" TEXT="mvn clean package -DskipTests">
+<icon BUILTIN="tag_green"/>
+</node>
+<node CREATED="1771683157943" MODIFIED="1771683160301" TEXT="cp target/xpath-workbench-1.0-SNAPSHOT.jar ~/apps/xpath-workbench/xpath-workbench.jar">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+</map>
